5b8e363f666eec31a8d349a82080a22204aad4b5
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-12-15 08:05:16 -0700

    uncommented for readability

--- inUnisionTechInterview.js
+++ inUnisionTechInterview.js
@@ -1,51 +1,51 @@
 //Part 1
 
-// const val={};
-// class RyanCache{
-//     get(key){          
-//         if(!val.hasOwnProperty(key)){
-//             //check db table size, using below fake BE Postgres function (getSize()), against val size to see if val needs to be updated.
-//             //This is assuming that the db is maintaining all of the values.
-            // if(dataSource.getSize() === Object.keys(val).length){
-            //     return false;
-            // } else {
-            //     Object.assign(val,dataSource.fetch());
-            // }
-//         }
-//     }  
-// }
+const val={};
+class RyanCache{
+    get(key){          
+        if(!val.hasOwnProperty(key)){
+            //check db table size, using below fake BE Postgres function (getSize()), against val size to see if val needs to be updated.
+            //This is assuming that the db is maintaining all of the values.
+            if(dataSource.getSize() === Object.keys(val).length){
+                return false;
+            } else {
+                Object.assign(val,dataSource.fetch());
+            }
+        }
+    }  
+}
 
 //Part 2
 
-// class RyanCache{
-//     vals = [];
+class RyanCache{
+    vals = [];
     
-//     get(arrayOfKeys){
+    get(arrayOfKeys){
         
-//         let toFetch = [];
-//         let toReturn = [];
-//         //iterate over the passed in array of keys checking if they are contained in val. if they are then add the key and value to toReturn
-//         //otherwise add them to toFetch which will then be used to populate vals. 
-//         for(let i in arrayOfKeys){
-//             if(!vals.hasOwnProperty(i)){
-//                 toFetch.push(i)
-//             } else {
-//                 toReturn.push({i:arrayOfKeys[i]})
-//             }
-//         }
-//         if(toFetch.length>0){
-//             let addToVals = dataSource.fetch(...toFetch);//this assumes fetch takes an arry and returns a value for each found.
-//             addToVals.forEach(val=>{
-//                 vals.push(val);
-//                 toReturn.push(val);
-//             })
-//         }
-            // if(!(dataSource.getSize() === Object.keys(val).length){
-            //     Object.assign(val,dataSource.fetch());
-            // }
-//         return toReturn;     //toReturn was populated by the first for loop and then the second fetch and should have the values if 
-//     }                         //if they were in vals otherwise it would fetch using a new list of those missing and return the value or null.      
-// }
+        let toFetch = [];
+        let toReturn = [];
+        //iterate over the passed in array of keys checking if they are contained in val. if they are then add the key and value to toReturn
+        //otherwise add them to toFetch which will then be used to populate vals. 
+        for(let i in arrayOfKeys){
+            if(!vals.hasOwnProperty(i)){
+                toFetch.push(i)
+            } else {
+                toReturn.push({i:arrayOfKeys[i]})
+            }
+        }
+        if(toFetch.length>0){
+            let addToVals = dataSource.fetch(...toFetch);//this assumes fetch takes an arry and returns a value for each found.
+            addToVals.forEach(val=>{
+                vals.push(val);
+                toReturn.push(val);
+            })
+        }
+            if(!(dataSource.getSize() === Object.keys(val).length){
+                Object.assign(val,dataSource.fetch());
+            }
+        return toReturn;     //toReturn was populated by the first for loop and then the second fetch and should have the values if 
+    }                         //if they were in vals otherwise it would fetch using a new list of those missing and return the value or null.      
+}
 
 //Part 3
 
